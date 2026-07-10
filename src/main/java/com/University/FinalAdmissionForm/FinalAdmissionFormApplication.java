d42ef532e4b5bae7e3c28a725204e0e92f99cc45
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-07-10 10:45:00 +0530

    First Commit

---
+++ src/main/java/com/University/FinalAdmissionForm/FinalAdmissionFormApplication.java
@@ -0,0 +1,13 @@
+package com.University.FinalAdmissionForm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalAdmissionFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalAdmissionFormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/University/FinalAdmissionForm/FinalAdmissionFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.University.FinalAdmissionForm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalAdmissionFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
